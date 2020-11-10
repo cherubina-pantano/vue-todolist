c7135d46522724ad76a3674730de505d57a94012
cherubina-pantano <cherubina.pantano@gmail.com>
2020-11-11 00:20:49 +0100

    keyup--show/hidden class--details

--- js/main.js
+++ js/main.js
@@ -8,29 +8,19 @@ const app = new Vue({
       {text: 'Fare il bucato'},
       {text: 'Andare in banca'},
     ],
-    // show: false,
 
   },
 
   methods: {
     addNewTodo() {
       this.todos.push({text: this.todo});
+      this.todo = '';
     },
 
     deleteTodo(index) {
       this.todos.splice(index, 1);
     },
 
-    // empityList() {
-    //   if (this.todos.leght == 0) {
-    //     this.show = true;
-    //     console.log('Lista vuota');
-    //   }
-    //   else {
-    //     this.show = false;
-    //     console.log('todos' + todos);
-    //   }
-    // }
   }
 
 });
