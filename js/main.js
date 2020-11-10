f62882fba4648d2b66e23a3117a0aca26c07fb64
cherubina-pantano <cherubina.pantano@gmail.com>
2020-11-10 20:24:31 +0100

    Add & Delete item to-do

--- js/main.js
+++ js/main.js
@@ -1,11 +1,36 @@
 const app = new Vue({
   el: '#app',
   data: {
+    todo: '',
+    indexTodo: 0,
     todos: [
       {text: 'Fare la spesa'},
       {text: 'Fare il bucato'},
       {text: 'Andare in banca'},
     ],
+    // show: false,
 
   },
+
+  methods: {
+    addNewTodo() {
+      this.todos.push({text: this.todo});
+    },
+
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    },
+
+    // empityList() {
+    //   if (this.todos.leght == 0) {
+    //     this.show = true;
+    //     console.log('Lista vuota');
+    //   }
+    //   else {
+    //     this.show = false;
+    //     console.log('todos' + todos);
+    //   }
+    // }
+  }
+
 });
